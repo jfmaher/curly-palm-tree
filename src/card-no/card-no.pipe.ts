b0eb67454b205464a81310d42962128c8b1d6d4a
James Maher <maher.j@live.com>
2023-11-06 17:28:35 +0000

    Added e2e validation tests

--- src/card-no/card-no.pipe.ts
+++ src/card-no/card-no.pipe.ts
@@ -1,10 +1,10 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 
 @Injectable()
 export class CardNoPipe implements PipeTransform {
   transform(value: string) {
     if (value.match(/^\d{16}$/) === null)
-      throw new Error('card number format wrong');
+      throw new BadRequestException('card number format wrong');
     return value;
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,7 +15,39 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200);
+  it('GET /credit-cards', () => {
+    request(app.getHttpServer()).get('/credit-cards').expect(200);
   });
+  describe('POST /credit-cards', () => {
+    it('fails validation', () => {
+      request(app.getHttpServer())
+        .post('/credit-cards')
+        .send({
+          name: 'test',
+          accountIdentifier: 'test',
+          limit: 1000,
+          cardNo: '4018888881881',
+          cardType: 'VISA',
+        })
+        .expect(400);
+    });
+    it('succeeds validation', () => {
+      request(app.getHttpServer())
+        .post('/credit-cards')
+        .send({
+          name: 'test',
+          accountIdentifier: 'test',
+          limit: 1000,
+          cardNo: '4012888888881881',
+          cardType: 'VISA',
+        })
+        .expect(200);
+    });
+  });
+  it.todo('GET /credit-cards/:id');
+  it.todo('PUT /credit-cards/:id');
+  it.todo('PATCH /credit-cards/:id');
+  it.todo('DELETE /credit-cards/:id');
+  it.todo('POST /credit-cards/:cardNo/charge');
+  it.todo('POST /credit-cards/:cardNo/credit');
 });
